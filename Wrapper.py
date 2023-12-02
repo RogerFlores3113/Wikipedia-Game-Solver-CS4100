491c89c84863fc0b54e0c7780d28df22bc2d5552
Roger Flores <flores.r@northeastern.edu>
2023-12-02 18:52:11 -0500

    Fixed Wrapper.py
    Less optimal but it should actually work now

--- Wrapper.py
+++ Wrapper.py
@@ -2,59 +2,65 @@ from Scraper import get_wikipedia_links_with_similarity
 import wikipediaapi
 import heapq
 
-
-def find_path_to_target(start_title: str, target_title: str, max_iters=15, initial_similarity_bound=-0.3):
+def find_path_to_target(start_title, target_title, max_iters=15):
     """
     Find a path from a start Wikipedia page to a target page using cosine similarity.
-    """
 
+    :param start_title: Title of the starting Wikipedia page
+    :param target_title: Title of the target Wikipedia page
+    :param max_iters: Maximum number of iterations (default 6)
+    :return: List of page titles representing the path taken or None if not found
+    """
     wiki_wiki = wikipediaapi.Wikipedia(user_agent="Wikipedia game solution grapher (flores.r@northeastern.edu)", language="en")
     target_page = wiki_wiki.page(target_title)
     if not target_page.exists():
         return None
 
-    priority_queue = [(-1, start_title, [])]  # Max heap
+    # Priority queue (negative min heap = max heap), contains starting page at first
+    priority_queue = [(-1, start_title, [])]  # heap element is: (negative similarity, title, path)
+
     visited = set()
-    fifth_highest_similarity = initial_similarity_bound
-    final_path = []  # Variable to store the final path
 
     for iteration in range(max_iters):
         if not priority_queue:
-            print("Priority queue is empty before reaching max iterations.")
             break
 
+        # Pop the page with the highest similarity, skipping over those we've already seen
         _, current_title, path = heapq.heappop(priority_queue)
-        if current_title in visited:
-            continue
+        
+        while current_title in visited:
+             _, current_title, path = heapq.heappop(priority_queue)
+
+
+        print(f"For Iteration {iteration}, visiting page: {current_title}")
+
         visited.add(current_title)
 
-        # Update the path
-        current_path = path + [current_title]
-
-        print(f"Iteration: {iteration}, Current page: {current_title}")  
-        if current_title == target_title:
-            return current_path
-            break
-
         current_page = wiki_wiki.page(current_title)
-        if target_title in current_page.links:
-            return current_path + [target_title]
-            break
 
+        # check if we've reached the target
+        if current_title.casefold() == target_title.casefold():
+            print("found the target page! Here is the path:")
+            return [start_title] + path
+        
+        if target_title in wiki_wiki.page(current_title).links:
+            print("found the target page! Here is the path:")
+            return [start_title] + path + [target_title]
+        
+
+        # otherwise get all links with their cosine similarities
         links_with_similarity = get_wikipedia_links_with_similarity(current_page, target_page)
 
+        # And add the linked pages to the frontier
         for link_title, (similarity, _) in links_with_similarity.items():
-            if link_title not in visited and -similarity > fifth_highest_similarity:
-                new_path = current_path + [link_title]
+            if link_title not in visited:
+                new_path = path + [link_title]
                 heapq.heappush(priority_queue, (-similarity, link_title, new_path))
 
-        # Update the minimum similarity threshold
-        if len(priority_queue) > :
-            priority_queue = heapq.nlargest(5, priority_queue)
-            heapq.heapify(priority_queue)
-            fifth_highest_similarity = -priority_queue[-1][0]
+    # if we didnt find a path
+    print("Unable to find a full path! Here is the path explored thus far:")
+    return [start_title] + path
 
-    return current_path
 
 
  
@@ -62,7 +68,7 @@ def find_path_to_target(start_title: str, target_title: str, max_iters=15, initi
 
 if (__name__ == "__main__"):
 
-    paths = find_path_to_target("Industrial Era", "Fruit")
+    paths = find_path_to_target("Storm", "Hawaii")
     print("returned path:")
     for i, p in enumerate(paths):
         print(f"step {i}, page: {p}")
